2c31279fa725081f3fab8bfa7bc8149ddc2f54a0
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-02-10 23:52:09 +0500

    update

---
+++ Practice_class3.py
@@ -0,0 +1,12 @@
+#Practice Program:
+num = int(input("Enter the number to check the number is even or odd:"))
+if(num%2==0):
+    print("Number is even.")
+else:
+    print("Number is odd.")    
+
+#Another Practice Program:
+num1= int(input("Enter value a:"))
+num2= int(input("Enter value b:"))
+num3= int(input("Enter value c:"))
+
