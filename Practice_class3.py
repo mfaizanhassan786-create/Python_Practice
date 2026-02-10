0bd0535584650578ee89e86d8fe29a9ac7187a46
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-02-11 00:03:31 +0500

    Update

--- Practice_class3.py
+++ Practice_class3.py
@@ -1,23 +1,25 @@
-#Practice Program:3
-num = int(input("Enter the number to check the number is even or odd:"))
-if(num%2==0):
-    print("Number is even.")
-else:
-    print("Number is odd.")    
+# #Practice Program:3
+# num = int(input("Enter the number to check the number is even or odd:"))
+# if(num%2==0):
+#     print("Number is even.")
+# else:
+#     print("Number is odd.")    
 
-#Another Practice Program:
-a= int(input("Enter value a:"))
-b= int(input("Enter value b:"))
-c= int(input("Enter value c:"))
-if(a >= b and a >= c ):
-    print("First is greater",a)
-elif(b > c):
-        print("Second is greater",b)
-else:
-    print("Third id Greater",c)
+# #Another Practice Program:
+# a= int(input("Enter value a:"))
+# b= int(input("Enter value b:"))
+# c= int(input("Enter value c:"))
+# if(a >= b and a >= c ):
+#     print("First is greater",a)
+# elif(b > c):
+#         print("Second is greater",b)
+# else:
+#     print("Third id Greater",c)
 
 #Another Practice:
 
 x = int(input("Enter number:"))
 if(x % 7 == 0):
-    
+    print("multiple of 7")
+else:
+    print("not a multiple")    
