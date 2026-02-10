e2d8fbde95b5301b1ac758e71a84a31d41391775
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-02-10 22:54:15 +0500

    Improved examples in Variables

---
+++ Practice_class1.py
@@ -0,0 +1,19 @@
+print("Hello World")
+print("This is my First Practice Program.")
+a=10
+b=20
+print("The sum of a and b is:",a+b)
+print("The difference of a and b is:",a-b)
+print("The product of a and b is:",a*b)
+print("The division of a and b is:",a/b)
+print("The modulus of a and b is:",a%b)
+print("The exponentiation of a and b is:",a**b)
+print("The floor division of a and b is:",a//b)
+
+#Input in the Python.
+
+name=input("Enter your name:")
+age=input("Enter your age:")
+print("Name:",name)
+print("Age:",age)
+print("Thanks for Input")
---
+++ Practice_class2.py
@@ -0,0 +1,12 @@
+#some are the Practice of the Python:
+
+name=input("Enter your name: ")
+print("length of the name is",len(name))
+
+var= "Today the $ is the equal to 1$=280 pk"
+print(var.count("$"))
+
+#Find the letter "a" in the string:
+
+name=input("Enter your name: ")
+print(name.find("a"))
